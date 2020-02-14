d1945ab233d26fa88774c27b44b3cb8bfb18601d
Sidhant Bhavnani <sidhant.gla@gmail.com>
2020-02-14 11:38:15 +0000

    [C08] Passed tests.

--- rango/views.py
+++ rango/views.py
@@ -26,7 +26,7 @@ def add_category(request):
         form = CategoryForm(request.POST)
     if form.is_valid():
         form.save(commit=True)
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
     else:
         print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
@@ -40,7 +40,7 @@ def add_page(request, category_name_slug):
 
     # You cannot add a page to a Category that does not exist... DM
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
